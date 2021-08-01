ece75a964fa24beae91184ea483e19639648f758
Shishir Pandey <shishir.py@gmail.com>
2021-07-31 20:11:54 -0400

    fixes #43: added a third case to binary:match (#44)
    * fixes #43: added a third case to binary:match
    
    * removed the extra case

--- src/chumak_subscriptions.erl
+++ src/chumak_subscriptions.erl
@@ -55,9 +55,9 @@ peer_match(Subscriptions, PeerPid, FirstPart) ->
 
                   (PeerSubscription) ->
                       case binary:match(FirstPart, PeerSubscription) of
-                          nomatch ->
-                              false;
+                          {0, _} ->
+                              true;
                           _ ->
-                              true
+                              false
                       end
               end, PeerSubscriptions).
